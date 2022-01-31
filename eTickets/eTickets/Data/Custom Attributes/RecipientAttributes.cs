fda2174be4f00afdcafa187a538fd68ba03d2451
Geno Enchev <gnenchev99@gmail.com>
2022-02-01 01:17:04 +0200

    Addead Custom Attributes

---
+++ eTickets/eTickets/Data/Custom Attributes/RecipientAttributes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eTickets.Data.Custom_Attributes
+{
+    public class RecipientAttributes
+    {
+        public class DateGreaterThanTodayAttribute : ValidationAttribute
+        {
+            public DateGreaterThanTodayAttribute() : base("{0}Date should be greater than current date")
+            {
+
+            }
+            public override bool IsValid(object value)
+            {
+                DateTime propValue = Convert.ToDateTime(value);
+                if (propValue >= DateTime.Now)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
